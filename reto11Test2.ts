4684beb14ea810e8bc9952707980f0b928816019
Javier Garrido <javier.garrido.aguilera@gmail.com>
2022-05-14 12:50:48 +0200

    Se añaden campo price

--- reto11Test2.ts
+++ reto11Test2.ts
@@ -1,8 +1,8 @@
 import {Mobile} from './reto11'
 
-let mobile1:Mobile = new Mobile("Nokia3210", "3210", "Nokia", 64, "Rojo", false, 1);
-let mobile2:Mobile = new Mobile("Iphone3G", "3G", "Iphone", 128, "Gris", false, 1);
-let mobile3:Mobile = new Mobile("Samsug Galaxy 10", "10", "Samsung", 128, "Amarillo", false, 2);
+let mobile1:Mobile = new Mobile("Nokia3210", "3210", "Nokia", 64, "Rojo", false, 1, 350);
+let mobile2:Mobile = new Mobile("Iphone3G", "3G", "Iphone", 128, "Gris", false, 1, 660);
+let mobile3:Mobile = new Mobile("Samsug Galaxy 10", "10", "Samsung", 128, "Amarillo", false, 2, 750);
 
 console.log(mobile1.getName());
 console.log(mobile1.getModel());
@@ -11,6 +11,7 @@ console.log(mobile1.getSdsize());
 console.log(mobile1.getColor());
 console.log(mobile1.getIs5G());
 console.log(mobile1.getCameranumber());
+console.log(mobile1.getPrice());
 
 console.log(mobile2.getModel());
 console.log(mobile2.getTrademark());
@@ -18,6 +19,7 @@ console.log(mobile2.getSdsize());
 console.log(mobile2.getColor());
 console.log(mobile2.getIs5G());
 console.log(mobile2.getCameranumber());
+console.log(mobile2.getPrice());
 
 console.log(mobile3.getModel());
 console.log(mobile3.getTrademark());
@@ -25,6 +27,7 @@ console.log(mobile3.getSdsize());
 console.log(mobile3.getColor());
 console.log(mobile3.getIs5G());
 console.log(mobile3.getCameranumber());
+console.log(mobile3.getPrice());
 
 mobile1.setIs5G(true);
 mobile1.setCameraNumber(4);
