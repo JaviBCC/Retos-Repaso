fccadc46fb2af9bf25ef5181cbe824c01f5a3027
Javier Garrido <javier.garrido.aguilera@gmail.com>
2022-05-14 17:30:13 +0200

    Subo mitad Hoja2 de retos repaso y clase Mobile replicada

--- reto12.ts
+++ reto12.ts
@@ -1,4 +1,4 @@
-import {Mobile} from "./reto11"
+import {Mobile} from "./reto11-copy"
 
 
 // CREAR UNA CLASE QUE SE DENOMINE MOBILELIBRARY QUE CONTENGA LOS SIGUIENTES ATRIBUTOS PRIVADOS:
@@ -64,16 +64,42 @@ export class MobileLibrary {
 
     // METODOS
 
-    totalPriceCalculation() {
+    totalPriceCalculation():number {
 
         let sumPrice = 0;
 
         for(let i=0; i < this.mobiles.length; i++) {
-            sumPrice += this.totalPrice;
+
+            sumPrice += this.mobiles[i].getPrice();
+
         }
 
-        return sumPrice
+        return sumPrice 
     }
 
-
 }
+   
+    
+    
+
+    // let mobile1:Mobile = new Mobile("Nokia3210", "3210", "Nokia", 64, "Rojo", false, 1, 350);
+    // let mobile2:Mobile = new Mobile("Iphone3G", "3G", "Iphone", 128, "Gris", false, 1, 660);
+    // let mobile3:Mobile = new Mobile("Samsug Galaxy 10", "10", "Samsung", 128, "Amarillo", false, 2, 750);
+
+    // let arrMobile:Mobile[] = [mobile1,mobile2,mobile3];
+
+
+    // let mobile4:Mobile  = new Mobile("iPhone 13 Pro Max", "13 Pro Max", "Apple", 256, "Silver", true, 2, 1300);
+    // let mobile5:Mobile  = new Mobile("Samsung Galaxy S22 Ultra", "Galaxy S22 Ultra", "Samsung", 512, "Negro", true, 4, 1380);
+    // let mobile6:Mobile  = new Mobile("Huawei Mate 40 Pro", "Mate 40 Pro", "Huawei", 256, "Gold", true, 4, 1090);
+    // let mobile7:Mobile  = new Mobile("Pocophone x2 pro", "x2 pro", "Pocophone", 128, "Yellow", false, 2, 200);
+    
+    // let miStock:Mobile[] = [mobile4, mobile5, mobile6, mobile7];
+
+    // let myMobileLibrary:MobileLibrary = new MobileLibrary("Movistar Center", "Madrid", miStock); 
+    
+    // console.log(myMobileLibrary.totalPriceCalculation());
+
+
+
+
